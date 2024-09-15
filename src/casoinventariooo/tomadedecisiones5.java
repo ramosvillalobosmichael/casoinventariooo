b10679c2786b406d0ef72b56a97fba7f9ffad5a3
ramos villalobos michael <>
2024-09-15 17:30:42 -0500

    ramosvillalobosmichael : realizo el proceso del punto 5 toma de decisiones

---
+++ src/casoinventariooo/tomadedecisiones5.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nuevoproyectodesafio;
+
+/**
+ *
+ * @author domingoram
+ */
+public class tomadedecisiones5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        5. Toma de Decisiones:
+
+        El programa deberá evaluar si el valor total del inventario de un producto, después de aplicar los descuentos, supera los 500 soles.Si es así, deberá mostrar el siguiente mensaje:
+
+        Atención: Se recomienda reducir el inventario ya que el valor total supera los 500 soles.
+        
+        */
+        double totalCosto5=600;
+        
+        if (totalCosto5 > 500) {
+            System.out.println("Atención: Se recomienda reducir el inventario ya que el valor total supera los 500 soles.");
+        }
+        
+        
+    }
+    
+}
